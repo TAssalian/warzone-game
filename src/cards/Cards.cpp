38f19c1f1da220d75a8155d00ae5489a9825cb50
Kira <kafountain5@gmail.com>
2026-02-09 08:57:04 -0500

    more progress

--- src/cards/Cards.cpp
+++ src/cards/Cards.cpp
@@ -1,4 +1,6 @@
 #include "Cards.h"
+#include <iostream>
+#include <assert.h>
 
 void Card::play()
 {
@@ -9,13 +11,47 @@ Card::Card(CardType type) : type(type)
 {
 }
 
-void Deck::draw()
+Card::~Card()
+{	
+}
+
+Card& Deck::draw()
 {
-	
+	assert(cards != nullptr && !cards->empty());
+
+	Card* drawnCard = std::move(cards->back());
+	cards->pop_back();
+
+	return *drawnCard;
 }
 
 Deck::Deck(std::vector<Card*>& cards) : cards(&cards)
 {
-};
+}
 
+Deck::~Deck()
+{
+	for (auto card : *cards) {
+		delete card;
+	}
 
+	delete cards;
+}
+
+Hand::Hand(Deck* deck)
+{
+	cards = new std::vector<Card*>();
+	// Fill the hand with cards from the deck
+	for (int i = 0; i < handSize; i++) {
+		cards->push_back(&deck->draw());
+	}
+}
+
+Hand::~Hand()
+{
+	for (auto card : *cards) {
+		delete card;
+	}
+
+	delete cards;
+}
